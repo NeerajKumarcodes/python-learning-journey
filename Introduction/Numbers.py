3015b4846abe3ba29d091a9d41f25f9ebd9f32bf
Neeraj Kumar <neerajkumar171001@gmail.com>
2024-06-01 00:32:03 +0530

    Update and rename Builtin Functions.py to Numbers.py

---
+++ Introduction/Numbers.py
@@ -0,0 +1,23 @@
+x = 10
+y = 12.4
+z = 18j
+w = "Hello, World!"
+
+#numbers
+#converting numbers
+a = float(x)
+b = int(y)
+c = complex(x) 
+#complex numbers cannot be converted.
+
+#random() function generates a random number in a given range.
+import random
+print(random.randrange(3, 9))  # random.randrange(start, stop, step) start and step are optional, stop is required.
+
+#casting is used for scenarios when we want to specify the type of a variable.
+p = float(3.14)
+q = str("s1")
+
+
+
+
