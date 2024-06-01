817f75a79347c3c2b6ab9aac71535033b89e2f38
Neeraj Kumar <neerajkumar171001@gmail.com>
2024-06-01 22:15:05 +0530

    Create Boolean.py

---
+++ Introduction/Boolean.py
@@ -0,0 +1,13 @@
+#Boolean data
+
+#For checking whether a statement is true or false.
+print(10 > 9) #Prints True
+print(10 == 9) #Prints False
+print(10 < 9) #Prints False
+
+#bool() function evaluates any value and returns True or False.
+print(bool("Hello"))
+print(bool(15))
+
+#Most data is true for example any number except zero, any non-empty string, list, dictionary, set and tuple.
+#A class or a function (will be discussed in later topics) which returns 0 or False can also evaluate to False upon being called.
