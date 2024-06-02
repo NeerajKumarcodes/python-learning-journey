d9f226ff9bac18faf502a6d6b4bdf6c30ffb4e2a
Neeraj Kumar <neerajkumar171001@gmail.com>
2024-06-02 22:55:22 +0530

    Update List.py

--- Introduction/List.py
+++ Introduction/List.py
@@ -29,5 +29,43 @@ _list.append("Asteroid Belt")
 _extendedList = ["Jupiter", "Saturn", "Uranus", "Neptune"]
 _list.extend(_extendedList) #Any iterable object such tuples, sets, dictionaries can be appended.
 
+#To remove intems, remove() method is used.
+_list.remove("Asteroid Belt")
+#pop() method removes specified index
+_list.remove(0) #del keyword can also be used for this purpose. del _list[0]
+#clear method can empty the entire list.
+
+#Looping through list is possible.
+
+#for loop
+for x in _list:
+  print(x)
+
+for i in range(len(_list)):
+  print(_list[i])
+
+#while loop
+while i < len(_list):
+  print(_list[i])
+  i = i + 1
+
+#List Sorting.
+
+#Sorting Alphanumerically requires sort() method.
+_list.sort() #This is in ascending manner.
+_list.sort(reverse = True) #This is in descending manner.
+#By default sort() is case sensitive.
+#To make it case insensitive str.lower is used as a key function. sort(key = str.lower)
+
+#List reversing.
+_list.reverse()
+
+#List Copy.
+
+#copy() method can be used.
+newList = _list.copy()
+#built-in list() method can also be used.
+_newList = list(_list)
+
 
 
